baa786d1dbbb8762f4d6ca8db3ebd53fe42886a7
JuliMart <juli.martinezm@duocuc.cl>
2023-09-13 16:50:54 -0300

    Correccion

--- ShV/src/app/home/home.page.ts
+++ ShV/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePageComponent {
+  nombreUsuario: string = ''; // Variable para capturar el nombre de usuario
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  navegarAMain() {
+    // Verifica si se ha ingresado un nombre de usuario
+    if (this.nombreUsuario.trim() !== '') {
+      // Navega a la página /main y pasa el nombre de usuario como parámetro
+      this.router.navigate(['/main'], { queryParams: { usuario: this.nombreUsuario } });
+    }
+  }
 }
