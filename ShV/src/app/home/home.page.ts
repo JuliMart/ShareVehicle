3611132ce15c7b0718d33fd5860d05cc9e3c64e7
JuliMart <juli.martinezm@duocuc.cl>
2023-09-21 10:19:04 -0300

    v1

--- ShV/src/app/home/home.page.ts
+++ ShV/src/app/home/home.page.ts
@@ -1,22 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePageComponent {
-  nombreUsuario: string = ''; // Variable para capturar el nombre de usuario
+export class HomePage {
 
-  constructor(private router: Router) {}
+  constructor(
+    ) {}
+  
 
-  navegarAMain() {
-    // Verifica si se ha ingresado un nombre de usuario
-    if (this.nombreUsuario.trim() !== '') {
-      // Navega a la página /main y pasa el nombre de usuario como parámetro
-      this.router.navigate(['/main'], { queryParams: { usuario: this.nombreUsuario } });
-    }
-  }
 }
