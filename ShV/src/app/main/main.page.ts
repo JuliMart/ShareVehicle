798ba944f583a12e65819076a1980cbae5f7d976
JuliMart <juli.martinezm@duocuc.cl>
2023-10-02 23:01:42 -0300

    Se agrega saludo bienvenida

--- ShV/src/app/main/main.page.ts
+++ ShV/src/app/main/main.page.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
+  usuario: string = ''; // Agrega una propiedad para almacenar el nombre de usuario
 
   constructor() { }
 
   ngOnInit() {
+    // Obtener el nombre de usuario almacenado en el almacenamiento local
+    this.usuario = localStorage.getItem('usuario') || '';
   }
 
 
