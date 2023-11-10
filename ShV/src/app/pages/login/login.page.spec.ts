728fb5ed61b5941d9b4736432b438e9512873ce4
JuliMart <juli.martinezm@duocuc.cl>
2023-11-10 09:55:03 -0300

    mergeConProblemas

--- ShV/src/app/pages/login/login.page.spec.ts
+++ ShV/src/app/pages/login/login.page.spec.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-=======
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
->>>>>>> 8a04bccfade98ba203b815198442f549e4cccee5
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
